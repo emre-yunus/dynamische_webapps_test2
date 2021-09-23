a3d483f26caabf2c0f82d5cbb7da3ac9b999851c
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 00:42:21 +0200

    Card component correcte onClick allen

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 export function Card(props) {
     const {title, children, extraClass} = props;
     const cardClass = extraClass===undefined ? "card" : "card " + extraClass;
-    return <div className={cardClass} onClick={() => alert(title || children)}>
+    return <div className={cardClass} onClick={() => alert(title || (typeof(children)==="object" ? children[0].props.children : children))}>
         {title && <h4>{title}</h4>}
         {children}
     </div>
