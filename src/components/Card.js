3ecb71456259b5712dd1858c928217009300faaf
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 00:31:30 +0200

    Card component met onClick title of children

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 export function Card(props) {
     const {title, children, extraClass} = props;
     const cardClass = ("card " + extraClass).trim();
-    return <div className={cardClass} onClick={() => alert(children)}>
+    return <div className={cardClass} onClick={() => alert(title || children)}>
         {title && <h4>{title}</h4>}
         {children}
     </div>
