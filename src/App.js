dfffe659b86ec3046e8c883a7da998101f61f4c4
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-23 16:30:11 +0200

    oefeningen nummers > 6 en nummers * 2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import {Numbers} from "./components/Numbers";
 
 function App() {
   return (
-    <Numbers title="Nummers" numbers={NUMBER_DATA} />
+      <div>
+        <Numbers title="Nummers" numbers={NUMBER_DATA} />
+        <Numbers title="Nummers groter dan 6" numbers={NUMBER_DATA.filter(n => n > 6)} />
+        <Numbers title="Nummers x 2" numbers={NUMBER_DATA.map(n => n * 2)} />
+      </div>
   );
 }
 
