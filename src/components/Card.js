46b8f0f46714b5aa16cf3391bab0cf8aec37f824
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 00:38:01 +0200

    Card component correctie extraClass undefined

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 export function Card(props) {
     const {title, children, extraClass} = props;
-    const cardClass = ("card " + extraClass).trim();
+    const cardClass = extraClass===undefined ? "card" : "card " + extraClass;
     return <div className={cardClass} onClick={() => alert(title || children)}>
         {title && <h4>{title}</h4>}
         {children}
