13a67bae1b2a024799281c56f55b67bb5c19fe65
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 01:04:24 +0200

    EventDemo link is clicked alert + stoppropagation

--- src/components/EventDemo.js
+++ src/components/EventDemo.js
@@ -2,8 +2,10 @@ import {Section} from "./Section";
 
 export function EventDemo() {
     return <Section title="Events">
-        <div style={{backgroundColor: "pink"}}>
-            <a href="http://www.thomasmore.be" onClick={(e) => {e.preventDefault();alert('link is clicked');}}>dit is een link met een href</a>
+        <div style={{backgroundColor: "pink"}} onClick={() => alert("div is clicked")}>
+            <a href="http://www.thomasmore.be" onClick={(e) => {e.preventDefault();e.stopPropagation();alert('link is clicked');}}>dit is een link met een href</a>
+            &nbsp;- en -&nbsp;
+            <button onClick={(e) => {e.stopPropagation();alert("button is clicked")}}>dit is een button</button>
         </div>
     </Section>
 }
