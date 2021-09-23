d5ef4face0fdc38333378b4b086949def73fec3e
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 00:56:50 +0200

    EventDemo link is clicked alert + preventDefault

--- src/components/EventDemo.js
+++ src/components/EventDemo.js
@@ -3,7 +3,7 @@ import {Section} from "./Section";
 export function EventDemo() {
     return <Section title="Events">
         <div style={{backgroundColor: "pink"}}>
-            <a href="http://www.thomasmore.be" onClick={() => alert('link is clicked')}>dit is een link met een href</a>
+            <a href="http://www.thomasmore.be" onClick={(e) => {e.preventDefault();alert('link is clicked');}}>dit is een link met een href</a>
         </div>
     </Section>
 }
