ac02eaf6379a73f6be6cec88c10b9af4bde36f54
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 13:32:02 +0200

    DtateDemo met toggle favoriet

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -1,9 +1,21 @@
 import {useState} from "react";
 import {Section} from "./Section";
+import {Numbers} from "./Numbers";
 
 export function StateDemo() {
     const [isOn, setIsOn] = useState(false);
     const [counter, setCounter] = useState(0);
+    const [favoriteNumbers, setFavoriteNumbers] = useState([]);
+
+    const isCounterFavorite = favoriteNumbers.includes(counter);
+    const toggleFavoriteButtonText = !isCounterFavorite ? counter + " is ook een favoriet" : counter + " is niet langer een favoriet";
+    const toggleFavoriteNumber = () => {
+        if (isCounterFavorite) {
+            setFavoriteNumbers(favoriteNumbers.filter(n => n!==counter));
+        } else {
+            setFavoriteNumbers([...favoriteNumbers, counter]);
+        }
+    }
 
     return <Section title="state">
         <h3>de huidige waarde van state is: {isOn ? "ON" : "OFF"}</h3>
@@ -18,5 +30,8 @@ export function StateDemo() {
         <button onClick={() => setCounter(counter + 1)}>+</button>
         <button onClick={() => setCounter(counter - 1)}>-</button>
         <p>de huidige waarde van counter is {counter}</p>
+        <hr />
+        <Numbers title="Mijn favorieten" numbers={favoriteNumbers} />
+        <button onClick={() => toggleFavoriteNumber()}>{toggleFavoriteButtonText}</button>
     </Section>
 }
