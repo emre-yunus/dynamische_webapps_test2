fc5f74791cdefac1462ee0d07f235aad15233681
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 16:00:16 +0200

    PersonBrowser previous en next buttons

--- src/components/PersonBrowser.js
+++ src/components/PersonBrowser.js
@@ -6,7 +6,22 @@ export function PersonBrowser(props) {
     const { persons } = props;
     const [ shownPerson, setShownPerson ] = useState(persons[0]);
 
+    const searchPerson = (next) => {
+        const shownKey = persons.indexOf(persons.find(p => p.id===shownPerson.id));
+        if (next) {
+            if (shownKey < persons.length - 1) return persons[shownKey + 1];
+            return persons[0];
+        } else {
+            if (shownKey > 0) return persons[shownKey - 1];
+            return persons[persons.length - 1];
+        }
+    }
+
     return <Section title="Person browser">
-        <Person person={shownPerson} />
+        <div className="personWithButtons">
+            <button onClick={() => setShownPerson(searchPerson(false))}>&lt;</button>
+            <Person person={shownPerson} />
+            <button onClick={() => setShownPerson(searchPerson(true))}>&gt;</button>
+        </div>
     </Section>
 }
