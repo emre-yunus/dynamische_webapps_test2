f3144aba2af27b33765e4074d7e0dd54a0105fdd
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 13:00:57 +0200

    StateDemo met button om true te zetten

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -1,9 +1,10 @@
 import {useState} from "react";
 import {Section} from "./Section";
 
-export function StateDemo(props) {
+export function StateDemo() {
     const [isOn, setIsOn] = useState(false);
     return <Section title="state">
         <h3>de huidige waarde van state is: {isOn ? "ON" : "OFF"}</h3>
+        <button onClick={() => setIsOn(true)}>verander de state in true</button>
     </Section>
 }
