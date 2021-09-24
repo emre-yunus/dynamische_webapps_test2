3cdbd5cde31c73d31e0b945ff09c1b7afc6f9f31
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-24 13:09:03 +0200

    StateDemo met extra counter state

--- src/components/StateDemo.js
+++ src/components/StateDemo.js
@@ -3,11 +3,20 @@ import {Section} from "./Section";
 
 export function StateDemo() {
     const [isOn, setIsOn] = useState(false);
+    const [counter, setCounter] = useState(0);
+
     return <Section title="state">
         <h3>de huidige waarde van state is: {isOn ? "ON" : "OFF"}</h3>
         <button onClick={() => setIsOn(true)}>verander de state in true</button>
         <button onClick={() => setIsOn(false)}>verander de state in false</button>
         <button onClick={() => setIsOn(!isOn)}>toggle state</button>
         <p>de huidige waarde van state is: {isOn ? "ON" : "OFF"}</p>
+        <hr />
+        <h3>de huidige waarde van counter is {counter}</h3>
+        <button onClick={() => setCounter(counter + 1)}>verhoog</button>
+        <button onClick={() => setCounter(counter - 1)}>verlaag</button>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => setCounter(counter - 1)}>-</button>
+        <p>de huidige waarde van counter is {counter}</p>
     </Section>
 }
