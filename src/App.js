0c75522a2818ee9372aea5f7933149ad492c655c
marcdc-tm <marc.decaluwe@thomasmore.be>
2021-09-29 20:59:00 +0200

    fragment introductie

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import {StateDemo} from "./components/StateDemo";
 
 function App() {
   return (
-      <div>
+      <>
         <Numbers title="Nummers" numbers={NUMBER_DATA} />
         <Numbers title="Nummers groter dan 6" numbers={NUMBER_DATA.filter(n => n > 6)} />
         <Numbers title="Nummers x 2" numbers={NUMBER_DATA.map(n => n * 2)} />
@@ -25,7 +25,7 @@ function App() {
         <Cities title="Steden waar personen wonen" cities={citiesFromPersons(PERSON_DATA)} />
         <EventDemo />
         <StateDemo />
-      </div>
+      </>
   );
 }
 
